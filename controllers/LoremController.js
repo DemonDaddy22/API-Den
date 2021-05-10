ad295efb4e5a0bee6b99cfe4c1881bf0648aecf0
Rohan Gupta <guptarohan369@gmail.com>
2021-05-11 01:17:13 +0530

    Throw bad request error if count does not exist or is less than 1

--- controllers/LoremController.js
+++ controllers/LoremController.js
@@ -2,11 +2,13 @@ const { generateLoremIpsum } = require('../services/loremIpsum.js');
 
 const getLoremIpsum = (req, res, next) => {
     const { q, count, startWithLorem = false } = req.query;
+    if (isNaN(count) || count <= 0)
+        return res.status(400).send({ err: new Error('Count must be an integer greater than 0') });
     try {
         const data = generateLoremIpsum(q, typeof count === 'number' ? count : parseInt(count), startWithLorem);
         res.status(200).send({ data });
     } catch (err) {
-        res.status(400).send({ err });
+        res.status(500).send({ err });
     }
 };
 
