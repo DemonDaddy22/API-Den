6f608e8f5397b89dd727ce0befd595084c15608d
Rohan Gupta <guptarohan369@gmail.com>
2021-05-12 01:54:08 +0530

    Add type check on startWithLorem

--- controllers/LoremController.js
+++ controllers/LoremController.js
@@ -1,11 +1,15 @@
-const { generateLoremIpsum } = require('../services/loremIpsum.js');
+const { generateLoremIpsum } = require('../services/LoremIpsum.js');
 
 const getLoremIpsum = (req, res, next) => {
     const { q, count, startWithLorem = false } = req.query;
     if (isNaN(count) || count <= 0)
         return res.status(400).send({ err: new Error('Count must be an integer greater than 0') });
     try {
-        const data = generateLoremIpsum(q, typeof count === 'number' ? count : parseInt(count), startWithLorem);
+        const data = generateLoremIpsum(
+            q,
+            typeof count === 'number' ? count : parseInt(count),
+            typeof startWithLorem === 'boolean' ? startWithLorem : startWithLorem === 'true'
+        );
         res.status(200).send({ data });
     } catch (err) {
         res.status(500).send({ err });
